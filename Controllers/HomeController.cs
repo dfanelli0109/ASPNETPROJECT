6fc9698e063c3e853bc8670fdc5007836fffa16a
Daniel Fanelli <daniel.r.fanellijr@gmail.com>
2020-08-02 04:05:05 -0700

    updated project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyRSVP.Models;
+
+namespace PartyRSVP.Controllers {
+    public class HomeController : Controller {
+        public ViewResult Index() {
+            return View("Index");
+        }
+
+         public ViewResult Contact() {
+            return View("Contact");
+        }
+
+        public ViewResult About(){
+            return View("About");
+        }
+
+
+        public ViewResult Projects(){
+            return View("Projects");
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            // Todo: Store guest response, covered in later lessons
+            return View("Thanks", guestResponse);
+        }
+    }
+}
